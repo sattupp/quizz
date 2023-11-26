a3b55b5fb3083649f18b1859604090288be82124
sattupp <141204030+sattupp@users.noreply.github.com>
2023-11-27 00:01:40 +0530

    Update newscriptt.js

--- newscriptt.js
+++ newscriptt.js
@@ -2,74 +2,135 @@ const questions=[
 
   
   {
-    question: "hum charo me sbse chutiya kon hai?",
+    question: "Which keyword is used to establish an inheritance relationship between classes in Java? ",
 
     answers:[
 
-      {text:"shruti",correct:false},
-      {text:"varshney",correct:true},
-      {text:"garvit",correct:false},
-      {text:"satya",correct:false},
+      {text:"implement",correct:false},
+      {text:"extends",correct:true},
+      {text:"inherit",correct:false},
+      {text:"interface",correct:false},
     ]
   },
 
 
   {
-    question :"sabse cute kon hai?" ,
+    question :"Inheritance in Java allows a class to:" ,
 
     answers:[
 
-      {text:"shruti",correct:false},
-      {text:"satya",correct:true},
-      {text:"varshney",correct:false},
-      {text:"garvit",correct:false},
+      {text:"Acquire properties and methods from another class ",correct:true},
+      {text:"Create an entirely new class ",correct:false},
+      {text:"Hide the implementation details of a class ",correct:false},
+      {text:"Implement interfaces",correct:false},
     ]
   },
   
 
   {
-    question :"sabse sundar kon?",
+    question :"	Which sorting algorithm has the best average-case time complexity and is often used as a subroutine in other sorting algorithms?",
 
     answers:[
 
       
-      {text:"shruti",correct:true},
-      {text:"garvit",correct:false},
-      {text:"varshney",correct:false},
-      {text:"satya",correct:false},
+      {text:"Insetion sort",correct:false},
+      {text:"Bubble sort",correct:false},
+      {text:"Selection sort",correct:false},
+      {text:"Merge sort",correct:true},
     ]
   },
   
 
   {
-    question :"sabse jyada preshan kon krta?" ,
+    question :"Which operation on arrays is NOT performed in constant time?" ,
 
     answers:[
 
-      {text:"garvit",correct:true},
-      {text:"shruti",correct:false},
-      {text:"satya",correct:false},
-      {text:"varshney",correct:false},
+      {text:"Accessing an element",correct:false},
+      {text:"Deleting an element",correct:false},
+      {text:"Inserting an element at the end",correct:false},
+      {text:"Finding the length of the array",correct:true},
     ]
   },
   
   {
-    question :"hmara mentor kon hai?" ,
+    question :"What is the purpose of the stack in the expression evaluation algorithm?" ,
 
     answers:[
 
-      {text:"gaurav nigam",correct:false},
-      {text:"ashish",correct:false},
-      {text:"sajaivir",correct:true},
-      {text:"ajit tomar",correct:false},
+      {text:"To store operators",correct:false},
+      {text:"To store operands",correct:false},
+      {text:"To store both operators and operands",correct:false},
+      {text:"To store intermediate results",correct:true},
     ]
   },
+  
+  {
+    question :"Which data structure is often used to implement a stack?" ,
+
+    answers:[
+
+      {text:"Linked List",correct:false},
+      {text:"Array",correct:true},
+      {text:"Tree",correct:false},
+      {text:"Queue",correct:false},
+    ]
+  },
+
+  {
+    question :"What is the purpose of the front and rear pointers in a queue?" ,
+
+    answers:[
+
+      {text:"To mark the start and end of the queue",correct:true},
+      {text:"To implement priority in the queue",correct:false},
+      {text:"To track the size of the queue",correct:false},
+      {text:"To manage linked lists",correct:false},
+    ]
+  },
+
+  {
+    question :"Which of the following is true for a binary search tree (BST)?" ,
+
+    answers:[
+
+      {text:"Left subtree of a node contains nodes with smaller values",correct:false},
+      {text:"Right subtree of a node contains nodes with larger values",correct:false},
+      {text:"Both A and B",correct:true},
+      {text:"Neither A nor B",correct:false},
+    ]
+  },
+
+  {
+    question :"What is the shape property of a binary heap?" ,
+
+    answers:[
+
+      {text:"Complete Binary Tree",correct:true},
+      {text:"Full Binary Tree",correct:false},
+      {text:"Perfect Binary Tree",correct:false},
+      {text:"Balanced Binary Tree",correct:false},
+    ]
+  },
+
+  {
+    question :"What is the worst-case time complexity of inserting an element into a hash table with open addressing?" ,
+
+    answers:[
+
+      {text:"O(1)",correct:false},
+      {text:"O(log N)",correct:false},
+      {text:"O(N)",correct:true},
+      {text:"O(N^2)",correct:false},
+    ]
+  }
+
+
   
 
   
 
 ];
-
 
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
